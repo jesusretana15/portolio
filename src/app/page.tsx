0cc5b4513b83ac0b093afa630d536ba9e60bab73
Jesus Retana Artavia <jesusalberto196868@gmail.com>
2025-06-02 23:16:35 -0600

    fixed issue with fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-2 text-gray-400 md:grid-cols-2 md:grid-rows-1 items-center justify-items-center h-screen bg-gradient-to-r from-stone-900 to-gray-900 p-0 m-0 pb-0 gap-2 sm:p-5  font-[family-name:var(--font-geist-sans)]">
       <div className=" grid grid-rows-[auto_1fr_auto] h-full w-full    ">
-        <div className="row-start-1  text-base  md:text-lg">
+        <div className="row-start-1  text-base  md:text-base">
           lang
         </div>
         <div className="row-start-2  grid items-center justify-center h-full w-full ">
@@ -43,25 +43,25 @@ export default function Home() {
         <section id="about" className="p-5   rounded-lg ">
           <h2 className="text-2xl font-bold pb-5">About</h2>
           <p className="text-base pb-2 ">I’m a passionate <strong className="font-extrabold text-slate-400"> web developer</strong> specializing in building  responsive, accessible, and user-friendly websites and web apps using <strong className="font-extrabold text-slate-400"> React, Next.js, Tailwind CSS, and Strapi</strong>. With a strong foundation in <strong className="font-extrabold text-slate-400"> HTML, CSS, and JavaScript</strong> , I create clean, maintainable code tailored to client needs, including bilingual and niche market projects.</p>
-          <p className="text-lg">Beyond technical skills, I bring strong <strong className="font-extrabold text-slate-400"> problem-solving abilities, attention to detail, and clear communication </strong> to every project. I’m dedicated to continuous learning and delivering high-quality work that helps businesses grow online. My goal is to combine creativity and technology to build impactful digital experiences.</p>
+          <p className="text-base">Beyond technical skills, I bring strong <strong className="font-extrabold text-slate-400"> problem-solving abilities, attention to detail, and clear communication </strong> to every project. I’m dedicated to continuous learning and delivering high-quality work that helps businesses grow online. My goal is to combine creativity and technology to build impactful digital experiences.</p>
         </section>
 
         <section id="projects" className="p-5">
           <h2 className="text-2xl font-bold pb-5">Projects</h2>
-          <p className="text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error reiciendis incidunt culpa vero beatae, amet animi, porro quod ratione voluptate consequuntur perspiciatis, laborum eligendi aliquid sequi dignissimos odit at. Architecto.</p>
-          <p className="text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error reiciendis incidunt culpa vero beatae, amet animi, porro quod ratione voluptate consequuntur perspiciatis, laborum eligendi aliquid sequi dignissimos odit at. Architecto.</p>
-          <p className="text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error reiciendis incidunt culpa vero beatae, amet animi, porro quod ratione voluptate consequuntur perspiciatis, laborum eligendi aliquid sequi dignissimos odit at. Architecto.</p>
-          <p className="text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error reiciendis incidunt culpa vero beatae, amet animi, porro quod ratione voluptate consequuntur perspiciatis, laborum eligendi aliquid sequi dignissimos odit at. Architecto.</p>
-          <p className="text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error reiciendis incidunt culpa vero beatae, amet animi, porro quod ratione voluptate consequuntur perspiciatis, laborum eligendi aliquid sequi dignissimos odit at. Architecto.</p>
-          <p className="text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error reiciendis incidunt culpa vero beatae, amet animi, porro quod ratione voluptate consequuntur perspiciatis, laborum eligendi aliquid sequi dignissimos odit at. Architecto.</p>
-          <p className="text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error reiciendis incidunt culpa vero beatae, amet animi, porro quod ratione voluptate consequuntur perspiciatis, laborum eligendi aliquid sequi dignissimos odit at. Architecto.</p>
+          <p className="text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error reiciendis incidunt culpa vero beatae, amet animi, porro quod ratione voluptate consequuntur perspiciatis, laborum eligendi aliquid sequi dignissimos odit at. Architecto.</p>
+          <p className="text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error reiciendis incidunt culpa vero beatae, amet animi, porro quod ratione voluptate consequuntur perspiciatis, laborum eligendi aliquid sequi dignissimos odit at. Architecto.</p>
+          <p className="text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error reiciendis incidunt culpa vero beatae, amet animi, porro quod ratione voluptate consequuntur perspiciatis, laborum eligendi aliquid sequi dignissimos odit at. Architecto.</p>
+          <p className="text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error reiciendis incidunt culpa vero beatae, amet animi, porro quod ratione voluptate consequuntur perspiciatis, laborum eligendi aliquid sequi dignissimos odit at. Architecto.</p>
+          <p className="text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error reiciendis incidunt culpa vero beatae, amet animi, porro quod ratione voluptate consequuntur perspiciatis, laborum eligendi aliquid sequi dignissimos odit at. Architecto.</p>
+          <p className="text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error reiciendis incidunt culpa vero beatae, amet animi, porro quod ratione voluptate consequuntur perspiciatis, laborum eligendi aliquid sequi dignissimos odit at. Architecto.</p>
+          <p className="text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error reiciendis incidunt culpa vero beatae, amet animi, porro quod ratione voluptate consequuntur perspiciatis, laborum eligendi aliquid sequi dignissimos odit at. Architecto.</p>
         </section>
 
         <section id="contact" className="p-5">
           <h2 className="text-2xl font-bold pb-5">Contact </h2>
-          <p className="text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error reiciendis incidunt culpa vero beatae, amet animi, porro quod ratione voluptate consequuntur perspiciatis, laborum eligendi aliquid sequi dignissimos odit at. Architecto.</p>
-          <p className="text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error reiciendis incidunt culpa vero beatae, amet animi, porro quod ratione voluptate consequuntur perspiciatis, laborum eligendi aliquid sequi dignissimos odit at. Architecto.</p>
-          <p className="text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error reiciendis incidunt culpa vero beatae, amet animi, porro quod ratione voluptate consequuntur perspiciatis, laborum eligendi aliquid sequi dignissimos odit at. Architecto.</p>
+          <p className="text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error reiciendis incidunt culpa vero beatae, amet animi, porro quod ratione voluptate consequuntur perspiciatis, laborum eligendi aliquid sequi dignissimos odit at. Architecto.</p>
+          <p className="text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error reiciendis incidunt culpa vero beatae, amet animi, porro quod ratione voluptate consequuntur perspiciatis, laborum eligendi aliquid sequi dignissimos odit at. Architecto.</p>
+          <p className="text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error reiciendis incidunt culpa vero beatae, amet animi, porro quod ratione voluptate consequuntur perspiciatis, laborum eligendi aliquid sequi dignissimos odit at. Architecto.</p>
         </section>
       </div>
 
